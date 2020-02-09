96452a7a9360f11e18532839977ed5d67b24f275
hakadu <35991070+hakadu@users.noreply.github.com>
2020-02-09 14:55:53 +0200

    Update helix_webhook.py

--- app/helix_webhook.py
+++ app/helix_webhook.py
@@ -4,7 +4,6 @@ import json
 import socket
 import requests
 import configparser
-import os 
 
 from flask.app import Flask, request
 
@@ -18,8 +17,8 @@ app = Flask(__name__)
 def push_event(body):
     body = '[' + body + ']'
     helix = config['HELIX CONNECTION']
-    headers = {'Content-Type': 'application/json', 'Authorization': 'apiKey ' + os.environ.get('APIKEY', '60ca36fc-6e7f-471c-a378-a5718b67ee0a') }
-    url = 'https://' + os.environ.get('HOSTNAME', 'clm-tlv-tsuw2v.bmc.com') + ":" + os.environ.get('PORT', '443') + '/tsws/api/v1.0/events'
+    headers = {'Content-Type': 'application/json', 'Authorization': 'apiKey ' + helix['helix.apikey']}
+    url = 'https://' + helix['helix.host'] + ":" + helix['helix.port'] + '/tsws/api/v1.0/events'
     req = requests.post(url, headers=headers, data=body, verify=False)
     if req.status_code == 200:
         print('OK: Pushing event:' + body)
@@ -33,15 +32,19 @@ def build_event_body(data):
         status = "OPEN"
         msg = ''
         if alert['annotations']:
-            if alert['annotations']['message']:
+            if alert['annotations'].get('message'):
                 msg = alert['annotations']['message']
             elif alert['annotations']['description']:
                 msg = alert['annotations']['description']
         if data['status'].lower() == 'resolved':
             status = 'CLOSE'
+        if alert['labels'].get('alert_name'):                                                                                                                                                                                 
+          alert_name = alert['labels'].get('alert_name')                                                                                                                                                                      
+        else:                                                                                                                                                                                                                 
+          alert_name = alert['labels'].get('alertname')
         body = {'source_identifier': data['receiver'], 'object_id': data['receiver'], 'object_class': 'Prometheus',
                 'severity': alert['labels']['severity'].upper(), 'class': 'Prometheus_Event',
-                'object': alert['labels']['alert_name'], 'msg': msg, 'status': status, 'url': alert['generatorURL'],
+                'object': alert_name, 'msg': msg, 'status': status, 'url': alert['generatorURL'],
                 'owner': data['receiver'], 'labels': str(alert['labels']), 'annotations': str(alert['annotations'])}
         host_body = {'source_hostname': socket.getfqdn(request.remote_addr), 'source_address': request.remote_addr}
         body['source_attributes'] = host_body
